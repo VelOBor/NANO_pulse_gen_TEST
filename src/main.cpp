5aeb18c97a6556bcbbe05106fdee33c5013e393f
Oleg <57217654+VelOBor@users.noreply.github.com>
2024-10-23 01:05:10 +0300

    Дров наломал
    Переименовал переменные для читабельности, изменил диапазон частот (но ничего не изменилось на выходе), убрал лишнее в сравнениях...

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int hall_1_pin = 2;  //выход датчик холла 1 -- hall effect senso
 int hall_2_pin = 3;  //выход датчик холла 2 -- hall effect sensor simulator 2
 
 int min_freq = 1; // установка минимальной частоты -- minimum frequency
-int hysteresis = 20; //гистерезис -- hysteresis
+int hysteresis = 10; //гистерезис -- hysteresis
 
-bool hall_sensor0, hall_sensor1; //переменная датчиков холла -- hall effect signal flags
+bool hall_sensor1, hall_sensor2; //переменная датчиков холла -- hall effect signal flags
 int but_state = 0;  //состояние кнопки -- button state
 
 int freq; //частота -- frequency
@@ -23,9 +23,9 @@ void setup() {
   pinMode(butpin, INPUT_PULLUP); //вход кнопки имитации остановки энкодеар -- brake button input
   pinMode(potpin, INPUT); //подключение потенциометра -- pot input
   digitalWrite(hall_1_pin, LOW); //устанавливает выход сигнала  имитация датчик холла 1 в низкий логический уровень -- sets output level for sensor simulator
-  hall_sensor0 = false; //устанавливает выход сигнала  имитация датчик холла 1 в низкий логический уровень -- sets flag for sensor simulator
+  hall_sensor1 = false; //устанавливает выход сигнала  имитация датчик холла 1 в низкий логический уровень -- sets flag for sensor simulator
   digitalWrite(hall_2_pin, LOW); //устанавливает выход сигнала  имитация датчик холла 2 в низкий логический уровень -- sets output level for sensor simulator
-  hall_sensor1 = false; //устанавливает выход сигнала  имитация датчик холла 2 в низкий логический уровень -- sets flag for sensor simulator
+  hall_sensor2 = false; //устанавливает выход сигнала  имитация датчик холла 2 в низкий логический уровень -- sets flag for sensor simulator
   //period = 50;  
 }
 
@@ -36,36 +36,44 @@ void loop() {
   if (but_state == 1) //проверка чтения кнопки если лог1 то читаем аналоговый вход, если лог0 не читаем -- checking for button state, 1 - proceed to read pot, 0 - exit
   {
     pot_val = analogRead(potpin); // чтение аналогового входа -- reading analog input
-    if (pot_val > (512+hysteresis)) {freq = map(pot_val, 512, 1023, 1, 140);} // диапазон чтения 1,1023 положение резюка -140, 140 частота в герцах -- remapping raw pot input into frequency in Hz
-    else if (pot_val < (512-hysteresis)) {freq = map(pot_val, 1, 512, -140, 1);}
-    else pot_val = 1;
+    if (pot_val > (512+hysteresis)) {freq = map(pot_val, (512+hysteresis), 1023, 1, 250);} // диапазон чтения 1,1023 положение резюка -140, 140 частота в герцах -- remapping raw pot input into frequency in Hz
+        else if (pot_val < (512-hysteresis)) {freq = map(pot_val, 1, (512-hysteresis), -250, -1);}
+    
     if (freq != 0){period = 1000 / abs(freq);} //расчет частоты -- calculating frequency
       else if (freq == 0) {freq = 1;}  //исключаем возможность деления на 0 -- attempting to mitigate division by 0
-    if (freq > (min_freq+hysteresis)) 
+    
+    if (freq > min_freq) 
     {
       delay(period/4);
-      hall_sensor0 = !hall_sensor0; //логика работы выходов -- output switching logic
-      digitalWrite(hall_1_pin, hall_sensor0);
+      hall_sensor1 = !hall_sensor1; //логика работы выходов -- output switching logic
+      digitalWrite(hall_1_pin, hall_sensor1);
 
       delay(period/4);
-      hall_sensor1 = !hall_sensor1;
-      digitalWrite(hall_2_pin, hall_sensor1);
+      hall_sensor2 = !hall_sensor2;
+      digitalWrite(hall_2_pin, hall_sensor2);
     } 
-    else if ( freq < (min_freq-hysteresis)) //почему меньше чем -мин_частота?! -- why less that -freq?! original "else if ( freq < (-(min_freq-hysteresis)))" was slowing down code
+    
+    else if (freq < min_freq) //почему меньше чем -мин_частота?! -- why less that -freq?! original "else if (freq < (-min_freq))"
     {
       delay(period/4);
-      hall_sensor1 = !hall_sensor1;
-      digitalWrite(hall_2_pin, hall_sensor1);
+      hall_sensor2 = !hall_sensor2;
+      digitalWrite(hall_2_pin, hall_sensor2);
 
       delay(period/4);
-      hall_sensor0 = !hall_sensor0;
-      digitalWrite(hall_1_pin, hall_sensor0);
-      
+      hall_sensor1 = !hall_sensor1;
+      digitalWrite(hall_1_pin, hall_sensor1);
     }
  
   }  
 
-else if (but_state == 0) {pot_val = 0; freq = 0;}
+else if (but_state == 0) {
+  pot_val = 0;
+  freq = 0; 
+  hall_sensor1 = 0; 
+  hall_sensor2 = 0;
+  digitalWrite(hall_1_pin, hall_sensor1);
+  digitalWrite(hall_2_pin, hall_sensor2);
+}
 
   Serial.print("Frequency: ");
   Serial.print(freq);
@@ -73,10 +81,10 @@ else if (but_state == 0) {pot_val = 0; freq = 0;}
   Serial.print(pot_val);
   Serial.print(" But_state: ");
   Serial.print(but_state);
-  Serial.print(" h0: ");
-  Serial.print(hall_sensor0);
   Serial.print(" h1: ");
   Serial.print(hall_sensor1);
+  Serial.print(" h2: ");
+  Serial.print(hall_sensor2);
   
   Serial.println(" ");
   
